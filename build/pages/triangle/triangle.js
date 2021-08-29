2c740f0b8684c08a93bf3c9a1c47e4259bbd5e3d
James Madden <jamesbmadden@gmail.com>
2021-08-29 16:51:31 -0700

    add new test and move build to new folder

---
+++ build/pages/triangle/triangle.js
@@ -0,0 +1,26 @@
+// create some constants for accessing WebGPU
+const canvas = document.querySelector('canvas') || document.createElement('canvas');
+const surface = canvas.getContext('webgpu');
+async function main() {
+    const adapter = await navigator.gpu.requestAdapter();
+    const device = await (adapter === null || adapter === void 0 ? void 0 : adapter.requestDevice());
+    // device must exist to continue
+    if (device === undefined)
+        return;
+    // configure the surface (canvas) for the device
+    surface === null || surface === void 0 ? void 0 : surface.configure({
+        device,
+        format: 'bgra8unorm'
+    });
+}
+// check if WebGPU is supported. Otherwise, show an error message
+if (navigator.gpu) {
+    main();
+}
+else {
+    const error = document.createElement('p');
+    error.className = 'errorMsg';
+    error.textContent = "It seems your browser doesn't support WebGPU. Make sure you're using the Nightly version of Firefox or the Canary version of Chrome or Edge and enable the WebGPU flag.";
+    document.body.replaceChild(error, canvas);
+}
+export {};
---
+++ src/pages/triangle/shader.wgsl.ts
@@ -0,0 +1,18 @@
+const shaderSource: string = `struct VertexOutput {
+  [[builtin(position)]] pos: vec4<f32>;
+  [[location(0)]] colour: vec4<f32>;
+}
+
+[[stage(vertex)]]
+fn vs_main([[location(0)]] pos: vec3<f32>, [[location(1)]] colour: vec3<f32>) -> VertexOutput {
+  var output: VertexOutput;
+  output.pos = vec4<f32>(pos, 0.0);
+  output.colour = vec4<f32>(colour, 1.0);
+}
+
+[[stage(fragment)]]
+fn fs_main(in: VertexOutput) -> [[location(0)]] vec4<f32> {
+  return in.colour;
+}`;
+
+export default shaderSource;
