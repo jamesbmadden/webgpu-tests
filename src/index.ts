57de413ae61477e5bbf10ee937cb7ac9f74811a2
James Madden <jamesbmadden@gmail.com>
2021-08-29 10:33:02 -0700

    Create project

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+console.log('test');
+
+const frame = document.querySelector('iframe') || document.createElement('iframe');
+
+// establish the click action of each item on the navbar
+document.querySelectorAll('li').forEach(link => {
+
+  // attach a click listener
+  link.addEventListener('click', () => {
+    // set the frame's src to the link's data-href attribute
+    frame.src = link.getAttribute('data-href') || '';
+  });
+
+});
---
+++ src/pages/render/render.ts
@@ -0,0 +1,52 @@
+// create some constants for accessing WebGPU
+const canvas = document.querySelector('canvas') || document.createElement('canvas');
+const surface = canvas.getContext('webgpu');
+
+// allow for await syntax
+async function main () {
+
+  // create adapter and device
+  const adapter = await navigator.gpu?.requestAdapter();
+  const device = await adapter?.requestDevice();
+
+  // make sure device exists
+  if (device === undefined) return;
+
+  // configure the surface (canvas context) to be able to draw from this device
+  surface?.configure({
+    device,
+    format: 'bgra8unorm'
+  });
+
+  // put the draw function inside the main function to allow use of scoped variables
+  async function draw () {
+
+    // make sure device exists
+    if (device === undefined) return;
+
+    // create the view to render to and make sure its not null
+    const view = surface?.getCurrentTexture().createView();
+    if (view === undefined) return;
+
+    // create command encoder and render pass
+    const commandEncoder = device.createCommandEncoder();
+    const renderPass = commandEncoder.beginRenderPass({
+      colorAttachments: [{
+        view,
+        loadValue: { r: 0.0, g: 0.5, b: 1.0, a: 1.0 },
+        storeOp: 'store'
+      }]
+    });
+
+    renderPass.endPass();
+    device.queue.submit([commandEncoder.finish()]);
+
+  }
+
+  requestAnimationFrame(() => {
+    draw();
+  });
+
+}
+
+main();
