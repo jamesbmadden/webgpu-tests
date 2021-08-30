f32130e66784adf76fa5b4a8326fc8a876cd8c7f
James Madden <jamesbmadden@gmail.com>
2021-08-29 17:29:32 -0700

    Complete new test

--- build/pages/triangle/shader.wgsl.js
+++ build/pages/triangle/shader.wgsl.js
@@ -1,13 +1,14 @@
 const shaderSource = `struct VertexOutput {
   [[builtin(position)]] pos: vec4<f32>;
   [[location(0)]] colour: vec4<f32>;
-}
+};
 
 [[stage(vertex)]]
 fn vs_main([[location(0)]] pos: vec3<f32>, [[location(1)]] colour: vec3<f32>) -> VertexOutput {
   var output: VertexOutput;
-  output.pos = vec4<f32>(pos, 0.0);
+  output.pos = vec4<f32>(pos, 1.0);
   output.colour = vec4<f32>(colour, 1.0);
+  return output;
 }
 
 [[stage(fragment)]]
--- build/pages/triangle/triangle.js
+++ build/pages/triangle/triangle.js
@@ -1,16 +1,105 @@
+// load shader code
+import shaderSource from './shader.wgsl.js';
 // create some constants for accessing WebGPU
 const canvas = document.querySelector('canvas') || document.createElement('canvas');
 const surface = canvas.getContext('webgpu');
+// make array of vertices
+const vertices = new Float32Array([
+    // pos          colour
+    0.0, 0.5, 0.0, 1.0, 0.0, 0.0,
+    -0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
+    0.5, -0.5, 0.0, 0.0, 0.0, 1.0
+]);
 async function main() {
     const adapter = await navigator.gpu.requestAdapter();
     const device = await (adapter === null || adapter === void 0 ? void 0 : adapter.requestDevice());
     // device must exist to continue
     if (device === undefined)
         return;
+    if (adapter === null)
+        return;
+    if (surface === null)
+        return;
+    // get the ideal format
+    const format = surface.getPreferredFormat(adapter);
     // configure the surface (canvas) for the device
-    surface === null || surface === void 0 ? void 0 : surface.configure({
-        device,
-        format: 'bgra8unorm'
+    surface === null || surface === void 0 ? void 0 : surface.configure({ device, format });
+    // create the shader module
+    const shader = device.createShaderModule({
+        code: shaderSource
+    });
+    // and the vertex buffer
+    const vertexBuf = device.createBuffer({
+        size: vertices.byteLength,
+        usage: GPUBufferUsage.VERTEX,
+        mappedAtCreation: true
+    });
+    new Float32Array(vertexBuf.getMappedRange()).set(vertices);
+    vertexBuf.unmap();
+    // create render pipeline
+    const pipelineLayout = device.createPipelineLayout({
+        bindGroupLayouts: []
+    });
+    const renderPipeline = device.createRenderPipeline({
+        layout: pipelineLayout,
+        vertex: {
+            module: shader,
+            entryPoint: 'vs_main',
+            buffers: [{
+                    arrayStride: 4 * 6,
+                    attributes: [
+                        {
+                            // position
+                            shaderLocation: 0,
+                            offset: 0,
+                            format: 'float32x3'
+                        },
+                        {
+                            // colour
+                            shaderLocation: 1,
+                            offset: 4 * 3,
+                            format: 'float32x3'
+                        }
+                    ]
+                }]
+        },
+        fragment: {
+            module: shader,
+            entryPoint: 'fs_main',
+            targets: [{ format }]
+        },
+        primitive: {
+            topology: 'triangle-list',
+            cullMode: 'back'
+        }
+    });
+    // okay now lets render
+    async function draw() {
+        // make sure device exists
+        if (device === undefined)
+            return;
+        // create view to render to and make sure its not underfined
+        const view = surface === null || surface === void 0 ? void 0 : surface.getCurrentTexture().createView();
+        if (view === undefined)
+            return;
+        // create command encoder and render pass
+        const commandEncoder = device.createCommandEncoder();
+        const renderPass = commandEncoder.beginRenderPass({
+            colorAttachments: [{
+                    view,
+                    loadValue: { r: 0, g: 0, b: 0, a: 1 },
+                    storeOp: 'store'
+                }]
+        });
+        // render :)
+        renderPass.setPipeline(renderPipeline);
+        renderPass.setVertexBuffer(0, vertexBuf);
+        renderPass.draw(3, 1);
+        renderPass.endPass();
+        device.queue.submit([commandEncoder.finish()]);
+    }
+    requestAnimationFrame(() => {
+        draw();
     });
 }
 // check if WebGPU is supported. Otherwise, show an error message
@@ -23,4 +112,3 @@ else {
     error.textContent = "It seems your browser doesn't support WebGPU. Make sure you're using the Nightly version of Firefox or the Canary version of Chrome or Edge and enable the WebGPU flag.";
     document.body.replaceChild(error, canvas);
 }
-export {};
