e491f8fcac546e018ca880db0c3da093abcb96f1
James Madden <jamesbmadden@gmail.com>
2022-09-24 16:17:06 -0400

    Update shaders to new format using decorators

--- src/pages/triangle/shader.wgsl.ts
+++ src/pages/triangle/shader.wgsl.ts
@@ -1,18 +1,18 @@
 const shaderSource: string = `struct VertexOutput {
-  [[builtin(position)]] pos: vec4<f32>;
-  [[location(0)]] colour: vec4<f32>;
+  @builtin(position) pos: vec4<f32>,
+  @location(0) colour: vec4<f32>
 };
 
-[[stage(vertex)]]
-fn vs_main([[location(0)]] pos: vec3<f32>, [[location(1)]] colour: vec3<f32>) -> VertexOutput {
+@vertex
+fn vs_main(@location(0) pos: vec3<f32>, @location(1) colour: vec3<f32>) -> VertexOutput {
   var output: VertexOutput;
   output.pos = vec4<f32>(pos, 1.0);
   output.colour = vec4<f32>(colour, 1.0);
   return output;
 }
 
-[[stage(fragment)]]
-fn fs_main(in: VertexOutput) -> [[location(0)]] vec4<f32> {
+@fragment
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
   return in.colour;
 }`;
 
